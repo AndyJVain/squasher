70fabc90179eeeedb1d688501ddbc1ee023e347c
Connor Carraher <connor@carraher.com>
2018-11-08 16:47:02 -0800

    using session varaiables

--- php/assign-bug.php
+++ php/assign-bug.php
@@ -20,8 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($role == "DEVELOPER") {
 
-        $queryState = "update squasher_reports set state = 'PENDING FIX VERIFICATION' where bug_id = $_GET['bug_id']";
-        $queryAssigned = "update squasher_reports set ASSIGNED = assigner where bug_id = $_GET['bug_id']";
+        $queryState = "update squasher_reports set state = 'PENDING FIX VERIFICATION' where bug_id = $bug_id";
+        $queryAssigned = "update squasher_reports set ASSIGNED = assigner where bug_id = $bug_id";
 
         $query = oci_parse($conn, $queryState);
         oci_execute($query);
@@ -33,14 +33,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($state == "PENDING BUG VERIFICATION") {
           if (isset($_POST['not_verified'])) {
               # Not Verified-button was clicked
-              $queryState = "update squasher_reports set state = 'BUG VERIFICATION FAILED' where bug_id = $_GET['bug_id']";
+              $queryState = "update squasher_reports set state = 'BUG VERIFICATION FAILED' where bug_id = $bug_id";
               $query = oci_parse($conn, $queryState);
               oci_execute($query);
           }
           elseif (isset($_POST['verified'])) {
               # Verified-button was clicked
-              $queryState = "update squasher_reports set state = 'PENDING DEVELOPER ASSIGNMENT' where bug_id = $_GET['bug_id']";
-              $queryAssigned = "update squasher_reports set ASSIGNED = andyj where bug_id = $_GET['bug_id']";
+              $queryState = "update squasher_reports set state = 'PENDING DEVELOPER ASSIGNMENT' where bug_id = $bug_id";
+              $queryAssigned = "update squasher_reports set ASSIGNED = andyj where bug_id = $bug_id";
 
               $query = oci_parse($conn, $queryState);
               oci_execute($query);
@@ -52,8 +52,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } elseif ($state == "PENDING FIX VERIFICATION") {
             if (isset($_POST['not_verified'])) {
                 # Not Verified-button was clicked
-                $queryState = "update squasher_reports set state = 'PENDING DEVELOPER ASSIGNMENT' where bug_id = $_GET['bug_id']";
-                $queryAssigned = "update squasher_reports set ASSIGNED = andyj where bug_id = $_GET['bug_id']";
+                $queryState = "update squasher_reports set state = 'PENDING DEVELOPER ASSIGNMENT' where bug_id = $bug_id";
+                $queryAssigned = "update squasher_reports set ASSIGNED = andyj where bug_id = $bug_id";
 
                 $query = oci_parse($conn, $queryState);
                 oci_execute($query);
@@ -65,8 +65,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             elseif (isset($_POST['verified'])) {
                 # Verified-button was clicked
                 # Verified-button was clicked
-                $queryState = "update squasher_reports set state = 'DONE' where bug_id = $_GET['bug_id']";
-                $queryAssigned = "update squasher_reports set ASSIGNED = done where bug_id = $_GET['bug_id']";
+                $queryState = "update squasher_reports set state = 'DONE' where bug_id = $bug_id";
+                $queryAssigned = "update squasher_reports set ASSIGNED = done where bug_id = $bug_id";
 
                 $query = oci_parse($conn, $queryState);
                 oci_execute($query);
@@ -79,8 +79,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     elseif($role == "MANAGER"){
         $assigned_developer = $_POST["assigned_developer"];
 
-        $queryState = "update squasher_reports set state = 'PENDING FIX DEVELOPMENT' where bug_id = $_GET['bug_id']";
-        $queryAssigned = "update squasher_reports set ASSIGNED = $assigned_developer where bug_id = $_GET['bug_id']";
+        $queryState = "update squasher_reports set state = 'PENDING FIX DEVELOPMENT' where bug_id = $bug_id;
+        $queryAssigned = "update squasher_reports set ASSIGNED = $assigned_developer where bug_id = $bug_id;
 
         $query = oci_parse($conn, $queryState);
         oci_execute($query);
