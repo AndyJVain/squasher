b0a5032e1a938b4b790b76040fd295a0fcb8af41
Andy Vainauskas <ajvainauskas@gmail.com>
2018-11-24 13:50:35 -0800

    made fields required

--- php/pages/create.php
+++ php/pages/create.php
@@ -22,15 +22,15 @@
                 <form method="post">
                     <div class="form-group">
                         <label for="username" class="blue-text">Username</label>
-                        <input type="username" class="form-control" id="username" aria-describedby="Username" placeholder="Enter Username" name="username">
+                        <input type="username" class="form-control" id="username" aria-describedby="Username" placeholder="Enter Username" name="username" required>
                     </div>
                     <div class="form-group">
                         <label for="password" class="blue-text">Password</label>
-                        <input type="password" class="form-control" id="password" placeholder="Create Password" name="password">
+                        <input type="password" class="form-control" id="password" placeholder="Create Password" name="password" required>
                     </div>
                     <div class="form-group">
                         <label for="email" class="blue-text">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email">
+                        <input type="email" class="form-control" id="email" placeholder="Enter Email" name="email" required>
                     </div>
                     <input type="submit" class="btn btn-primary blue" value="Create">
                     <a type="button" class="btn btn-secondary white" href="login.php">Cancel</a>
