897673865ef84e1605130396c5366a169b1f9696
Andy Vainauskas <ajvainauskas@gmail.com>
2018-11-24 13:14:06 -0800

    changed create page to php

--- php/pages/create.php
+++ php/pages/create.php
@@ -8,8 +8,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/styles.css">
-    <link rel="stylesheet" href="../css/create.css">
+    <link rel="stylesheet" href="../../css/styles.css">
+    <link rel="stylesheet" href="../../css/create.css">
 
     <title>Squasher - Create Account</title>
 </head>
