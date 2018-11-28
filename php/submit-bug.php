ac55afbbb3e62ed52c5bc708f28a0a623951cc42
Connor Carraher <connor@carraher.com>
2018-11-27 22:56:46 -0800

    N sapce trigger update

--- php/submit-bug.php
+++ php/submit-bug.php
@@ -61,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $reportNumber = $row_reportNumber[0];
 
     //update ReportNumber
-    $updateReportNumberQuery = "update SQUASHER_COUNTER set report_number = $reportNumber+1 where report_number = $reportNumber)";
+    $updateReportNumberQuery = "update squasher_counter set report_number = ($reportNumber+1) where report_number = $reportNumber)";
     $query = oci_parse($conn, $updateReportNumberQuery);
     oci_execute($query);
 
