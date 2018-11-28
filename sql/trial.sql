14b351b0ce86c866c47f168776711f257d2c3bea
Connor Carraher <connor@carraher.com>
2018-11-27 21:52:34 -0800

    different changes

--- sql/trial.sql
+++ sql/trial.sql
@@ -1,17 +1,15 @@
---trial
-
 --handles assigning bugs for verification ON INSERT
 CREATE or REPLACE TRIGGER bugAssignment_insert
   AFTER INSERT on squasher_reports
   FOR EACH ROW
-  WHEN(:new.ASSIGNED = 'assigner')
+  WHEN(new.ASSIGNED = 'assigner')
 DECLARE
   v_minAssigned NUMBER;
   v_Assignment VARCHAR(50);
 BEGIN
-  select MIN(NUM_ASSIGNED) into v_minAssigned from squasher_user;
+  select MIN(NUM_ASSIGNED) into v_minAssigned from squasher_user where ROLE = 'TESTER';
 
-  select username into v_Assignment from squasher_user where ROLE = 'TESTER' and NUM_ASSIGNED = v_minAssigned and ROWNUM <= 1;
+  select username into v_Assignment from squasher_user where ROLE = 'TESTER' and NUM_ASSIGNED = v_minAssigned and ROWNUM <= 1 and USERNAME != 'assigned';
 
   update squasher_reports set ASSIGNED = v_Assignment where bug_id = :new.bug_id;
 END;
--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,7 +1,7 @@
 
 --handles updating the number of assigned bugs a user has ON UPDATE
 CREATE or REPLACE TRIGGER update_numAssigned
-    AFTER UPDATE on squasher_reports
+    BEFORE UPDATE on squasher_reports
     FOR EACH ROW
 DECLARE
     v_num_old NUMBER;
@@ -21,7 +21,7 @@ show errors;
 
 --handles updating the number of assigned bugs a user has ON INSERTION
 CREATE or REPLACE TRIGGER insert_numAssigned
-    AFTER INSERT on squasher_reports
+    BEFORE INSERT on squasher_reports
     FOR EACH ROW
 DECLARE
     v_num_new NUMBER;
@@ -31,24 +31,6 @@ BEGIN
     select NUM_ASSIGNED into v_num_new from squasher_user where USERNAME = :new.ASSIGNED;
     update squasher_user set NUM_ASSIGNED = (v_num_new+1) where USERNAME = :new.ASSIGNED;
 
-END;
-/
-show errors;
-
---handles assigning bugs for verification ON INSERT
-CREATE or REPLACE TRIGGER bugAssignment_insert
-  AFTER INSERT on squasher_reports
-  FOR EACH ROW
-  WHEN(new.ASSIGNED = 'assigner')
-DECLARE
-  v_minAssigned NUMBER;
-  v_Assignment VARCHAR(50);
-BEGIN
-  select MIN(NUM_ASSIGNED) into v_minAssigned from squasher_user where ROLE = 'TESTER';
-
-  select username into v_Assignment from squasher_user where ROLE = 'TESTER' and NUM_ASSIGNED = v_minAssigned and ROWNUM <= 1 and USERNAME != 'assigned';
-
-  update squasher_reports set ASSIGNED = v_Assignment where bug_id = :new.bug_id;
 END;
 /
 show errors;
