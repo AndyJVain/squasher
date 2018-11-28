e694b5ac27378063bb480b0c9d6a2a6a3c6eb8cc
Connor Carraher <connor@carraher.com>
2018-11-27 19:52:33 -0800

    more changes

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -5,27 +5,27 @@ BEFORE
 INSERT OR UPDATE
 --OF ASSIGNED --colname
 ON squasher_reports
-REFERENCING OLD AS o NEW AS n
+--REFERENCING OLD AS o NEW AS n
 FOR EACH ROW
-WHEN (o.ASSIGNED != n.ASSIGNED) --we dont want the cases where person assigned has not changed
+WHEN (OLD.ASSIGNED != NEW.ASSIGNED) --we dont want the cases where person assigned has not changed
 DECLARE
    v_num_old NUMBER;
    v_num_new NUMBER;
 BEGIN
    --Executable-statements
    --get old.person's number of bugs assigned to them
-   select NUM_ASSIGNED into v_num_old from squasher_user where USERNAME = o.ASSIGNED;
+   select NUM_ASSIGNED into v_num_old from squasher_user where USERNAME = OLD.ASSIGNED;
 
    --get new.person's number of bugs assigned to them
-   select NUM_ASSIGNED into v_num_new from squasher_user where USERNAME = n.ASSIGNED;
+   select NUM_ASSIGNED into v_num_new from squasher_user where USERNAME = NEW.ASSIGNED;
 
    --old person now has one less bug assigned to them
    v_num_old := v_num_old - 1;
-   update squasher_user set NUM_ASSIGNED = (v_num_old) where USERNAME = o.ASSIGNED;
+   update squasher_user set NUM_ASSIGNED = (v_num_old) where USERNAME = OLD.ASSIGNED;
 
    --new person now has one more bug assigned to them
    v_num_new := v_num_new + 1;
-   update squasher_user set NUM_ASSIGNED = (v_num_new) where USERNAME = n.ASSIGNED;
+   update squasher_user set NUM_ASSIGNED = (v_num_new) where USERNAME = NEW.ASSIGNED;
 
 END;
 /
